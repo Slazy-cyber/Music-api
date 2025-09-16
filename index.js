b04ba37a185c05a7a0f9d62c160d64a7433206fc
Abdulsalam Abdulsalam <abdulsalamabdulsalam1234567@gmail.com>
2025-09-16 13:14:03 +0100

    new

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 
 
 const afroMusic = [
-    {artist:'Davido', songTitle:'10 Kilo',songUrl:'https://www.youtube.com/watch?v=62Zazi9KEJc'},
+    {artistImage:'https://dailypost.ng/wp-content/uploads/2024/10/Davido.jpg',artist:'Davido', songTitle:'10 Kilo',songUrl:'https://www.youtube.com/watch?v=62Zazi9KEJc'},
 
-    {artist:'Burnaboy', songTitle:'we pray',songUrl:'https://open.spotify.com/track/6xX6zmaXeKzB4u9TiM1XgU?si=8e1886bd7b6a4056'},
+    {artistImage:'https://i.scdn.co/image/ab67616100005174b4e44d0f4e3e47af2cf06f3f',artist:'Burnaboy', songTitle:'we pray',songUrl:'https://open.spotify.com/track/6xX6zmaXeKzB4u9TiM1XgU?si=8e1886bd7b6a4056'},
 
-    {artist:'Wizkid', songTitle:'forever be mine',songUrl:'https://open.spotify.com/track/1m7j6RWUxeBzTOY4hcWsoj?si=a6f262a875284ee5'}
+    {artistImage:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxJyOB0HEaN88bFUYzNbpTiMXPY9F0zAZdMA&s',artist:'Wizkid', songTitle:'forever be mine',songUrl:'https://open.spotify.com/track/1m7j6RWUxeBzTOY4hcWsoj?si=a6f262a875284ee5'}
     
 ]
 
